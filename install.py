ff9425b4ff3bf2fc76629acbb005c8af72f5e5e5
Bobby <s215818@dtu.dk>
2024-01-02 18:05:00 +0100

    Use install.py to get dependencies

---
+++ install.py
@@ -0,0 +1,12 @@
+import os
+import subprocess
+
+
+def install_pipreqs():
+    # install the following packages: pip install numpy scipy matplotlib seaborn pandas altair vega_datasets scikit-learn bokeh datashader holoviews wordcloud spacy
+    subprocess.call(['pip', 'install', 'numpy', 'scipy', 'matplotlib', 'seaborn', 'pandas', 'altair', 'vega_datasets', 'scikit-learn', 'bokeh', 'datashader', 'holoviews', 'wordcloud', 'spacy'])
+
+
+if __name__ == "__main__":
+    install_pipreqs()
+    
