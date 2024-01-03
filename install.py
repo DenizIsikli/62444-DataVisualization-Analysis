ef1173fded4f7af8fceabb8ec1baf6820b3ee1cb
Bobby <s215818@dtu.dk>
2024-01-03 09:52:58 +0100

    install.py update

--- install.py
+++ install.py
@@ -4,7 +4,7 @@ import subprocess
 
 def install_pipreqs():
     # install the following packages: pip install numpy scipy matplotlib seaborn pandas altair vega_datasets scikit-learn bokeh datashader holoviews wordcloud spacy
-    subprocess.call(['pip', 'install', 'numpy', 'scipy', 'matplotlib', 'seaborn', 'pandas', 'altair', 'vega_datasets', 'scikit-learn', 'bokeh', 'datashader', 'holoviews', 'wordcloud', 'spacy'])
+    subprocess.call(['pip', 'install', 'notebook', 'numpy', 'scipy', 'matplotlib', 'seaborn', 'pandas', 'altair', 'vega_datasets', 'scikit-learn', 'bokeh', 'datashader', 'holoviews', 'wordcloud', 'spacy'])
 
 
 if __name__ == "__main__":
