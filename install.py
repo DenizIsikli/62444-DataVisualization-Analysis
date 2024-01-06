b7a78aa4b9af24bf872dc4bfada57bb646b66191
Bobby <s215818@dtu.dk>
2024-01-06 20:07:05 +0100

    Weak warning

--- install.py
+++ install.py
@@ -3,7 +3,8 @@ import subprocess
 
 
 def install_pipreqs():
-    # install the following packages: pip install numpy scipy matplotlib seaborn pandas altair vega_datasets scikit-learn bokeh datashader holoviews wordcloud spacy
+    # Install the following packages:
+    # numpy scipy matplotlib seaborn pandas altair vega_datasets scikit-learn bokeh datashader holoviews wordcloud spacy
     subprocess.call(['pip', 'install', 'notebook', 'numpy', 'scipy', 'matplotlib', 'seaborn', 'pandas', 'altair', 'vega_datasets', 'scikit-learn', 'bokeh', 'datashader', 'holoviews', 'wordcloud', 'spacy'])
 
 
