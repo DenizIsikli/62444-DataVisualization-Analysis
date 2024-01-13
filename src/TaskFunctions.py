6d18e00d9345c274595c0376cd2aafb0d090f038
Bobby <s215818@dtu.dk>
2024-01-13 20:25:08 +0100

    TaskFunctions.py placed inside the src folder to combine all assignment tasks in functions

---
+++ src/TaskFunctions.py
@@ -0,0 +1,15 @@
+
+
+class TaskFunctions_1:
+    def placeholder(self):
+        pass
+
+
+class TaskFunctions_2:
+    def placeholder(self):
+        pass
+
+
+if __name__ == '__main__':
+    tf_1 = TaskFunctions_1()
+    tf_2 = TaskFunctions_2()
