a8eae120393ccec64ba3ac7fc74ecc193bc3e597
OscarMaxwell <72600165+OscarMaxwell@users.noreply.github.com>
2024-01-15 11:19:50 +0100

    Put assignment 5.4

--- install.py
+++ install.py
@@ -5,7 +5,7 @@ import subprocess
 def install_pipreqs():
     # Install the following packages:
     # numpy scipy matplotlib seaborn pandas altair vega_datasets scikit-learn bokeh datashader holoviews wordcloud spacy
-    subprocess.call(['pip', 'install', 'notebook', 'numpy', 'scipy', 'matplotlib', 'seaborn', 'pandas', 'altair', 'vega_datasets', 'scikit-learn', 'bokeh', 'datashader', 'holoviews', 'wordcloud', 'spacy', 'pyarrow', 'fastparquet'])
+    subprocess.call(['pip', 'install', 'notebook', 'numpy', 'scipy', 'matplotlib', 'seaborn', 'pandas', 'altair', 'vega_datasets', 'scikit-learn', 'bokeh', 'datashader', 'holoviews', 'wordcloud', 'spacy', 'pyarrow', 'fastparquet', 'plotly'])
 
 
 if __name__ == "__main__":
